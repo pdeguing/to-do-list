4ad6bf67e6e65d9ccab73a356e21affd7876a27f
Pierre-quentin Deguingand <pierrequentindeguingand@gmail.com>
2019-03-30 12:37:26 -0700

    Implemented basic functionalities

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import FlipMove from "react-flip-move";
 
 class TodoItems extends Component {
+	constructor(props) {
+		super(props);
+
+		this.createTasks = this.createTasks.bind(this);
+	}
+
+	delete(key) {
+		this.props.delete(key);
+	}
+
 	createTasks(item) {
-		return <li key={item.key}>{item.text}</li>
+		return <li onClick={() => this.delete(item.key)}
+				key={item.key}>{item.text}</li>
 	}
 
 	render() {
@@ -11,7 +23,9 @@ class TodoItems extends Component {
 
 		return (
 			<ul className="theList">
-				{listItems}
+				<FlipMove duration={250} easing="ease-out">
+					{listItems}
+				</FlipMove>
 			</ul>
 		);
 	}
